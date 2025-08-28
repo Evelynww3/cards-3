6324ccf1c2fbd17fc5885220d21b586129e10d99
Evelynww3 <evelyn.camargo.sampaio@escola.pr.gov.br>
2025-08-28 09:27:03 -0300

    oi

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Doença de todd',
+    'O que é essa doença?',
+    'A Doença de Todd é paralisia temporária após uma crise.'
+)
+
+criaCartao(
+    'Sintomas',
+    'Quais são os sintomas?',
+    'Paralisia temporária após uma crise epiléptica.'
+)
+
+criaCartao(
+    'Precauções',
+    'Pode ser evitada?,
+    'Não, mas controlar as crises ajuda.'
+)
+
+criaCartao(
+    'Tratamento',
+    'Como é tratada?',
+    'Normalmente, não requer tratamento.'
+)
